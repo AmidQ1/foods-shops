82960f07699a80110e6eb72dcaa9908d9d4998dc
Sofia <putlyuksophia@gmail.com>
2025-03-13 21:00:18 +0200

    format code

--- src/components/AddModal/Modal.jsx
+++ src/components/AddModal/Modal.jsx
@@ -3,14 +3,14 @@ import { Component } from "react";
 import style from "./Modal.module.css";
 import { VscChromeClose } from "react-icons/vsc";
 class Modal extends Component {
-  
-      state = {
+
+    state = {
         info: null,
-        test:'test',
-        isOpen:false
-       }
-    
-    getInformation =(event) =>{
+        test: 'test',
+        isOpen: false
+    }
+
+    getInformation = (event) => {
         event.preventDefault();
         const name = event.target.elements.productName.value;
         const photo = event.target.elements.productPhoto.value;
@@ -23,65 +23,65 @@ class Modal extends Component {
             productDescription: description
         }
         //this.setState({info: productInfo})
-      //{...{ info: null,test:'test'},...{info: productInfo}}
-      this.setState({info: productInfo})
-     
+        //{...{ info: null,test:'test'},...{info: productInfo}}
+        this.setState({ info: productInfo })
+
     }
     onCloseModal = () => {
-    const close = this.state.isOpen
-    const searchModal = document.querySelector("#modal")
-    console.log(searchModal)
-    if(!close){
-        searchModal.classList.add('hide-modal')
-        this.state.isOpen = false
-    }else {
-        searchModal.classList.remove('hide-modal')
-        this.state.isOpen = true
-    } 
+        const close = this.state.isOpen
+        const searchModal = document.querySelector("#modal")
+        console.log(searchModal)
+        if (!close) {
+            searchModal.classList.add('hide-modal')
+            this.state.isOpen = false
+        } else {
+            searchModal.classList.remove('hide-modal')
+            this.state.isOpen = true
+        }
     }
 
-   /*const testTask = (a,b) => {
-    const sum = a+b;
-    return sum;
-    }
-   //Явне повернення
+    /*const testTask = (a,b) => {
+     const sum = a+b;
+     return sum;
+     }
+    //Явне повернення
+ 
+ 
+ 
+    //Неявне повернення
+    //      const testTask = (a,b) => a+b;
+     */
 
 
 
-   //Неявне повернення
-   //      const testTask = (a,b) => a+b;
-    */
 
 
-
-
-
-    render(){
-    return (
-        <div className={style.backdrop} id="modal">
-            <div className={style.modal}>
-                <button type="button" onClick={this.onCloseModal}>
-                <VscChromeClose className={style.close}  />
-                </button>
-                <form onSubmit={this.getInformation}>
-                    <label>
-                        <input type="text" name="productName" placeholder="Name" />
-                    </label>
-                    <label>
-                        <input type="text" name="productPhoto" placeholder="Photo" />
-                    </label>
-                    <label>
-                        <input type="number" name="productPrice" placeholder="price" />
-                    </label>
-                    <label>
-                        <input type="text" name="productDescription" placeholder="description" />
-                    </label>
-                    <button type="submit">Add</button>
-                </form>
+    render() {
+        return (
+            <div className={style.backdrop} id="modal">
+                <div className={style.modal}>
+                    <button type="button" onClick={this.onCloseModal}>
+                        <VscChromeClose className={style.close} />
+                    </button>
+                    <form onSubmit={this.getInformation}>
+                        <label>
+                            <input type="text" name="productName" placeholder="Name" />
+                        </label>
+                        <label>
+                            <input type="text" name="productPhoto" placeholder="Photo" />
+                        </label>
+                        <label>
+                            <input type="number" name="productPrice" placeholder="price" />
+                        </label>
+                        <label>
+                            <input type="text" name="productDescription" placeholder="description" />
+                        </label>
+                        <button type="submit">Add</button>
+                    </form>
+                </div>
             </div>
-        </div>
-    )
-}
+        )
+    }
 }
 
 
