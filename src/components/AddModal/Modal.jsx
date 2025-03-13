0c8cae90c4aff4df4a5fe24d6da645be6215b326
Nazarius9273827 <nazarbenevskij@gmail.com>
2025-03-13 19:48:25 +0200

    modal layout

--- src/components/AddModal/Modal.jsx
+++ src/components/AddModal/Modal.jsx
@@ -1,26 +1,46 @@
-import React from "react";
+import React, { Component } from "react";
+import style from "./Modal.module.css";
+import { VscChromeClose } from "react-icons/vsc";
 class Modal extends React.Component {
     constructor(props){
-        
+       super(props)
+       this.state = {
+        info: null
+       }
+    }
+    getInformation(event){
+        event.preventDefault();
+        const name = event.target.elements.productName.value;
+        const photo = event.target.elements.productPhoto.value;
+        const price = event.target.elements.productPrice.value;
+        const description = event.target.elements.productDescription.value;
+        const productInfo = {
+            productName: name,
+            productPhoto: photo,
+            productPrice: price,
+            productDescription: description
+        }
+        // this.setState(productInfo)
     }
     render(){
 
     
     return (
-        <div>
-            <div>
-                <form onSubmit={this.props.openModal}>
+        <div className={style.backdrop}>
+            <div className={style.modal}>
+                <VscChromeClose className={style.close} />
+                <form onSubmit={this.getInformation}>
                     <label>
-                        <input type="text" placeholder="Name" />
+                        <input type="text" name="productName" placeholder="Name" />
                     </label>
                     <label>
-                        <input type="text" placeholder="Photo" />
+                        <input type="text" name="productPhoto" placeholder="Photo" />
                     </label>
                     <label>
-                        <input type="number" placeholder="price" />
+                        <input type="number" name="productPrice" placeholder="price" />
                     </label>
                     <label>
-                        <input type="text" placeholder="description" />
+                        <input type="text" name="productDescription" placeholder="description" />
                     </label>
                     <button type="submit">Add</button>
                 </form>
