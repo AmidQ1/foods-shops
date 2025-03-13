9f1dd17356f4616a44a87031af5bd3fd28a45306
Sofia <putlyuksophia@gmail.com>
2025-03-13 20:58:47 +0200

    changes Modal

--- src/components/AddModal/Modal.jsx
+++ src/components/AddModal/Modal.jsx
@@ -1,14 +1,16 @@
-import React, { Component } from "react";
+import React from "react";
+import { Component } from "react";
 import style from "./Modal.module.css";
 import { VscChromeClose } from "react-icons/vsc";
-class Modal extends React.Component {
-    constructor(props){
-       super(props)
-       this.state = {
-        info: null
+class Modal extends Component {
+  
+      state = {
+        info: null,
+        test:'test',
+        isOpen:false
        }
-    }
-    getInformation(event){
+    
+    getInformation =(event) =>{
         event.preventDefault();
         const name = event.target.elements.productName.value;
         const photo = event.target.elements.productPhoto.value;
@@ -20,15 +22,47 @@ class Modal extends React.Component {
             productPrice: price,
             productDescription: description
         }
-        // this.setState(productInfo)
+        //this.setState({info: productInfo})
+      //{...{ info: null,test:'test'},...{info: productInfo}}
+      this.setState({info: productInfo})
+     
     }
-    render(){
+    onCloseModal = () => {
+    const close = this.state.isOpen
+    const searchModal = document.querySelector("#modal")
+    console.log(searchModal)
+    if(!close){
+        searchModal.classList.add('hide-modal')
+        this.state.isOpen = false
+    }else {
+        searchModal.classList.remove('hide-modal')
+        this.state.isOpen = true
+    } 
+    }
 
-    
+   /*const testTask = (a,b) => {
+    const sum = a+b;
+    return sum;
+    }
+   //Явне повернення
+
+
+
+   //Неявне повернення
+   //      const testTask = (a,b) => a+b;
+    */
+
+
+
+
+
+    render(){
     return (
-        <div className={style.backdrop}>
+        <div className={style.backdrop} id="modal">
             <div className={style.modal}>
-                <VscChromeClose className={style.close} />
+                <button type="button" onClick={this.onCloseModal}>
+                <VscChromeClose className={style.close}  />
+                </button>
                 <form onSubmit={this.getInformation}>
                     <label>
                         <input type="text" name="productName" placeholder="Name" />
